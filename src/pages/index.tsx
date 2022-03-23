53e8fe3d311953ac00585c1302b7520d82fab735
Mikkel RINGAUD <mikkel@milescode.dev>
2022-03-23 15:38:36 +0100

    Made home page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,53 @@
 import { Link } from "react-router-dom";
 
+import { FaUserFriends, FaPlay } from "react-icons/fa";
+
+type HomeMenuSelectorItemProps = {
+  to: string,
+  title: string,
+  description: string,
+  iconElement: React.ReactNode
+};
+
+const HomeMenuSelectorItem = ({
+  to,
+  title,
+  description,
+  iconElement
+}: HomeMenuSelectorItemProps) => {
+  return (
+    <Link to={to} className="relative px-6 py-4 pr-12 transition-colors bg-zinc-900 bg-opacity-40 w-full rounded-md hover:bg-opacity-80">
+      <h2 className="text-xl">{title}</h2>
+      <p className="text-zinc-400">{description}</p>
+
+      <span className="absolute top-2 right-2 p-2 rounded-lg bg-zinc-900">{iconElement}</span>
+    </Link>
+  );
+};
+
 export default function HomePage () {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>This is the home page</p>
+    <main className="h-screen w-screen flex flex-col items-center justify-center gap-8">
+      <div className="text-center">
+        <h1 className="text-2xl">osu! web player</h1>
+        <p>Made with ❤️ by <a className="text-purple-300 hover:underline underline-offset-2" href="https://github.com/Vexcited" target="_blank" rel="noreferrer">Vexcited</a></p>
+      </div>
 
-      <Link to={"/singleplayer"}>Single Player</Link>
-      <Link to={"/multiplayer"}>Multi Player</Link>
-    </div>
+      <div className="grid place-items-center gap-2 mx-4">
+        <HomeMenuSelectorItem
+          to="/singleplayer"
+          title="Single Player"
+          description="Import maps and play them !"
+          iconElement={<FaPlay />}
+        />
+
+        <HomeMenuSelectorItem
+          to="/multiplayer"
+          title="Multiplayer"
+          description="Play with your friends !"
+          iconElement={<FaUserFriends />}
+        />
+      </div>
+    </main>
   );
 }
